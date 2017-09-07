43ce25140720bc19fb04f8371e71bb8877f6d5e3
Badajoz <sbadajoz@student.cccd.edu>
2017-09-07 15:13:44 -0700

    Add information logging

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/tapcounter/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/tapcounter/MainActivity.java
@@ -2,62 +2,36 @@ package edu.orangecoastcollege.cs273.sbadajoz.tapcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String tag = MainActivity.class.getSimpleName();
+
     private TextView mCountTextView;
 
     private Counter mCounter;
 
+    /**
+     * Called whenever teh TapCounter app is redrawn
+     *
+     * @param savedInstanceState A bundle of information from which previously saved state can be restored
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCounter = new Counter();
         mCountTextView = (TextView) findViewById(R.id.countTextView);
+
+        Log.i(tag, "onCreate successfully terminated");
     }
 
     public void tapButton(View view) {
         mCounter.setCount(mCounter.incrementCount());
-        mCountTextView.setText(String.valueOf(mCounter.getCount()))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        ;
+        mCountTextView.setText(String.valueOf(mCounter.getCount()));
     }
 }
 
